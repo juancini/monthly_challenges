67ea214d6add42df2262d15cc17cd91a2043bbfa
Juan Mancini <juani.mancini98@gmail.com>
2021-10-24 13:06:06 -0300

    Refactor URL dynamics

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<month>", views.monthly_challenge)
+    #En estos casos, el orden importa, si la 7 y 8 estuvieran al reves,
+    #pasaria siempre a monthly_challenge
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
 
+monthly_challenges = {
+    "january": "En Enero hay que comer vegano una vez al dia",
+    "february": "En Febrero hay que caminar 20 minutos todos los dias!",
+    "march": "En Marzo hay que tomar agua cada 2 horas!",
+    "april": "En Abril hay que hacer un chiste todos los dias!",
+    "may": "En Mayo hay que ordenar la pieza los martes y jueves!",
+    "june": "En Junio hay que aprender algo nuevo todos los dias!",
+    "july": "En Julio hay que comer frutas todos los dias",
+    "august": "En Agosto hay que meditar media hora todos los dias!",
+    "september": "En Septiembre hay que poner September 24/7",
+    "November": "En Noviembre hay que celebrar mi cumple todos los dias",
+    "December": "En Diciembre hay que hacer un regalo todos los dias"
+}
+
+def monthly_challenge_by_number(request, month):
+    months  = list(monthly_challenges.keys())
+    forward_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + forward_month)
+
 def monthly_challenge(request, month):
-    challengeText = None
-    if month == "january":
-        challengeText = "En Enero hay que comer vegano una vez al dia"
-    elif month == "february":
-        challengeText = "En Febrero hay que caminar 20 minutos todos los dias!"
-    elif month == 'march':
-        challengeText = "En Marzo hay que tomar agua cada 2 horas!"
-    else:
-        return HttpResponseNotFound("Esto no es un mes")
+    try:
+        challengeText = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound('Mes sin soporte')
     return HttpResponse(challengeText)
