fa7aa12676d83e922307a4963dfa7eb2645a0afe
Juan Mancini <juani.mancini98@gmail.com>
2021-10-24 12:20:05 -0300

    added Dynamic URLs

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Esto funciona")
+def monthly_challenge(request, month):
+    challengeText = None
+    if month == "january":
+        challengeText = "En Enero hay que comer vegano una vez al dia"
+    elif month == "february":
+        challengeText = "En Febrero hay que caminar 20 minutos todos los dias!"
+    elif month == 'march':
+        challengeText = "En Marzo hay que tomar agua cada 2 horas!"
+    else:
+        return HttpResponseNotFound("Esto no es un mes")
+    return HttpResponse(challengeText)
