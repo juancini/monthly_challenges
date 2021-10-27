ae9a0feaad45f9771cfe32083e9149bede573b44
Juan Mancini <juani.mancini98@gmail.com>
2021-10-27 14:20:04 -0300

    Added Templates

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, response
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -49,6 +50,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challengeText = monthly_challenges[month]
+        response_data = render_to_string("challenges/challenge.html")
+        return HttpResponse(response_data)
     except:
         return HttpResponseNotFound('Mes sin soporte')
-    return HttpResponse(challengeText)
