0bb2396b1e6eb4b51ea25b419250379c6de6c585
Juan Mancini <juani.mancini98@gmail.com>
2021-10-27 15:09:25 -0300

    added dynamic templates

--- challenges/views.py
+++ challenges/views.py
@@ -50,7 +50,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challengeText = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challengeText,
+            "month": month
+        })
     except:
         return HttpResponseNotFound('Mes sin soporte')
--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'challenges' #Best way to add templates
+    'challenges', #Best way to add templates
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
