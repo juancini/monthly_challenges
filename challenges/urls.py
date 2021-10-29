28124e82da92a14d5251c6df54f33e3b422ec670
Juan Mancini <juani.mancini98@gmail.com>
2021-10-28 21:10:03 -0300

    added inheritance templates

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     #En estos casos, el orden importa, si la 7 y 8 estuvieran al reves,
     #pasaria siempre a monthly_challenge
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
